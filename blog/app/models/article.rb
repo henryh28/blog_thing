4e51fbdb17cb4a8716c28e0a8452b9d0c2f01ccb
henryh28 <henry@henryhsu.net>
2014-05-03 12:55:55 -0700

    Implements comments

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :content
 
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 3 }
 end
