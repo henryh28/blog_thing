007167ac5f136670bb84e58157e4191204cf8364
henryh28 <henry@henryhsu.net>
2014-05-04 15:31:10 -0700

    Incorporates partials to implement comments

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :content
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :content, presence: true, length: { minimum: 3 }
